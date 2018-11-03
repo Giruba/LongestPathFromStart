f1230be93b667bde555d49681471ea26ae2776dd
gibeulah <gibeulah@microsoft.com>
2018-11-03 18:59:06 +0530

    First Commit

---
+++ LongestPathInMatrixFromStart/MatrixProcessor.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LongestPathInMatrixFromStart
+{
+    class MatrixProcessor
+    {
+        private int[,] matrix { get; set; }
+
+        public MatrixProcessor(int[,] matrix) {
+            this.matrix = matrix;
+        }
+
+        public void LongestPathInMatrixFromStart() {
+
+            int longestPathFromStart = 0;
+
+            int rows = matrix.GetLength(0);
+            int columns = matrix.GetLength(1);
+
+            int[,] dpTable = new int[rows, columns];
+
+            longestPathFromStart = LongestPathFromStart(0,0, dpTable);
+
+            Console.WriteLine("The Longest Path From Start of the matrix is "+longestPathFromStart);
+        }
+
+        private int LongestPathFromStart(int i, int j, int[,] dpTable) {
+                      
+
+            if (dpTable[i, j] == 0)
+            {
+                int maxSum = 0;
+
+                if (i == matrix.GetLength(1)-1 && j == matrix.GetLength(0)-1)
+                    return dpTable[i,j] = 1;
+
+                if (i == matrix.GetLength(1)-1 && j == matrix.GetLength(0)-1)
+                    maxSum = 1;
+
+                if (j + 1 < matrix.GetLength(1) && matrix[i, j] < matrix[i, j + 1])
+                {
+                    maxSum = 1 + LongestPathFromStart(i, j + 1, dpTable);
+                }
+
+                if (i + 1 < matrix.GetLength(0) && matrix[i, j] < matrix[i + 1, j])
+                {
+                    maxSum = Math.Max(maxSum, 1 + LongestPathFromStart(i + 1, j, dpTable));
+                }
+
+                dpTable[i, j] = maxSum;
+            }
+            return dpTable[i,j];
+        }
+    }
+}
--- LongestPathInMatrixFromStart/Program.cs
+++ LongestPathInMatrixFromStart/Program.cs
@@ -6,7 +6,40 @@ namespace LongestPathInMatrixFromStart
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Longest Path in a matrix from start!");
+            Console.WriteLine("-------------------------------------");
+
+            int[,] inputMatrix = GetInput();
+            MatrixProcessor matrixProcessor = new MatrixProcessor(inputMatrix);
+            matrixProcessor.LongestPathInMatrixFromStart();
+
+            Console.ReadLine();
+        }
+
+        private static int[,] GetInput() {
+            int[,] inputMatix = null;
+            Console.WriteLine("Enter the number of rows of the matrix");
+            try
+            {
+                int rows = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the number of colums of the matrix");
+                int columns = int.Parse(Console.ReadLine());
+                inputMatix = new int[rows, columns];
+                for (int i = 0; i < rows; i++) {
+                    Console.WriteLine("Enter the elements of row" + i + "separated by space");
+                    String rowElements = Console.ReadLine();
+                    if (!string.IsNullOrEmpty(rowElements)) {
+                        string[] elements = rowElements.Split(' ');
+                        for (int index = 0; index < elements.Length; index++) {
+                            inputMatix[i, index] = int.Parse(elements[index]);
+                        }
+                    }
+                }
+            }
+            catch (Exception exception) {
+                Console.WriteLine("The thrown exception is"+exception.Message);
+            }
+            return inputMatix;
         }
     }
 }
